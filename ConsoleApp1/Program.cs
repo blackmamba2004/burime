b21911707914976e0e9994aa5856615f279400d0
Symon <vasilsemen04@gmail.com>
2024-06-01 08:59:27 +0300

    first line

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x = double.Parse(Console.ReadLine());
+        }
+    }
+}
